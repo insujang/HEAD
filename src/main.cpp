1eac0ddc41034fd2ce52dc6dc554df50a1125e6d
Insu Jang <insujang@calab.kaist.ac.kr>
2016-11-03 15:03:47 +0900

    Fix undefined reference problem: extern "C"

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+extern "C" {
 #include "main.h"
 #include "string.h"
 #include "namespace.h"
@@ -13,81 +14,75 @@
 #include <sys/types.h>
 #include <pwd.h>
 //#include <string.h>
-
-#include <iostream>
-#include <vector>
-#include <string>
+}
 
 #include "levelDBWrapper.h"
 
-using namespace std;
-using std::vector;
 
 /*Main program!*/
 int
 main (int argc, char *argv[])
 {
-    cout << "Hello" << endl;
-    LevelDBWrapper* ldb;
-    string value("qwetrt");
+//    cout << "Hello" << endl;
+//    LevelDBWrapper* ldb;
+//    string value("qwetrt");
+//
+//    ldb = LevelDBWrapper::GetInstance();
+//
+//    string values2;
+//    ldb->writeDB("12345", value);
+//
+//    ldb->readDB("12345", values2);
+//
+//    return 0;
+        int     ret                       = -1;
+        char    namespace_path[FILE_SIZE] = "";
+        DIR     *dp                       = NULL;
 
-    ldb = LevelDBWrapper::GetInstance();
+        char 	namespace_dest[256];
+		char* 	dirhome;
 
-    string values2;
-    ldb->writeDB("12345", value);
+		/*
+			[chikdol]
+			Create directory if (~/cs710/) does not exist
+		*/
 
-    ldb->readDB("12345", values2);
+		if ((dirhome = getenv("HOME")) == NULL) {
+	    	dirhome = getpwuid(getuid())->pw_dir;
+		}
 
-    return 0;
-//        int     ret                       = -1;
-//        char    namespace_path[FILE_SIZE] = "";
-//        DIR     *dp                       = NULL;
-//
-//        char 	namespace_dest[256];
-//		char* 	dirhome;
-//
-//		/*
-//			[chikdol]
-//			Create directory if (~/cs710/) does not exist
-//		*/
-//
-//		if ((dirhome = getenv("HOME")) == NULL) {
-//	    	dirhome = getpwuid(getuid())->pw_dir;
-//		}
-//
-//		strcpy (namespace_dest,dirhome);
-//		strcat (namespace_dest,"/cs710");
-//
-//		dp = opendir(namespace_dest);
-//        if (NULL == dp) {
-//        	mkdir(namespace_dest, 0777);
-//        }
-//
-//
-//        //sprintf(namespace_path, "/var/lib/yadl");
-//        sprintf(namespace_path, "%s", namespace_dest);
-//        dp = opendir(namespace_path);
-//        if (NULL == dp) {
-//
-//                ret = mkdir(namespace_path, 0777);
-//                if (ret < 0) {
-//                        fprintf(stderr, "%s\n", strerror(errno));
-//                        goto out;
-//                }
-//                ret = create_default_namespace(namespace_path);
-//                if (ret < 0) {
-//                        fprintf(stderr, "%s\n", strerror(errno));
-//                        goto out;
-//                }
-//        }
-//        ret = start_program(argc, argv, namespace_path);
-//        if (ret == -1) {
-//                goto out;
-//        }
-//        ret = 0;
-//out:
-//        if (dp != NULL)
-//                closedir(dp);
-//        return ret;
+		strcpy (namespace_dest,dirhome);
+		strcat (namespace_dest,"/cs710");
 
+		dp = opendir(namespace_dest);
+        if (NULL == dp) {
+        	mkdir(namespace_dest, 0777);
+        }
+
+
+        //sprintf(namespace_path, "/var/lib/yadl");
+        sprintf(namespace_path, "%s", namespace_dest);
+        dp = opendir(namespace_path);
+        if (NULL == dp) {
+
+                ret = mkdir(namespace_path, 0777);
+                if (ret < 0) {
+                        fprintf(stderr, "%s\n", strerror(errno));
+                        goto out;
+                }
+                ret = create_default_namespace(namespace_path);
+                if (ret < 0) {
+                        fprintf(stderr, "%s\n", strerror(errno));
+                        goto out;
+                }
+        }
+        ret = start_program(argc, argv, namespace_path);
+        if (ret == -1) {
+                goto out;
+        }
+        ret = 0;
+out:
+        if (dp != NULL)
+                closedir(dp);
+        return ret;
 }
