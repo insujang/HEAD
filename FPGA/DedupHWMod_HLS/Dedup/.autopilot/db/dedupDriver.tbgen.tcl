82e0ed5fc0b3d9429fe786b6af2ed85fc164eca8
seikwon@kaist.ac.kr <seikwon@kaist.ac.kr>
2016-11-30 19:03:59 +0900

    modified:   dedup.cpp 	modified:   ../vivado_hls.app

--- FPGA/DedupHWMod_HLS/Dedup/.autopilot/db/dedupDriver.tbgen.tcl
+++
@@ -1,9 +0,0 @@
-set C_TypeInfoList {{ 
-"dedupDriver" : [[], { "return": [[], "void"]} , [{"ExternC" : 0}], [ {"inputData": [[], {"reference": "0"}] }, {"outputData": [[], {"reference": "1"}] }],[],""], 
-"0": [ "stream<char>", {"hls_type": {"stream": [[[[], {"scalar": "char"}]],"2"]}}], 
-"1": [ "stream<ap_item>", {"hls_type": {"stream": [[[[],"3"]],"2"]}}], 
-"3": [ "ap_item", {"struct": [[],[],[{ "data": [[], "4"]},{ "last": [[], "5"]}],""]}], 
-"4": [ "rtn", {"struct": [[],[],[{ "index": [[],  {"scalar": "int"}]},{ "hashData": [[],  {"scalar": "int"}]}],""]}], 
-"5": [ "ap_uint<1>", {"hls_type": {"ap_uint": [[[[], {"scalar": { "int": 1}}]],""]}}],
-"2": ["hls", ""]
-}}
