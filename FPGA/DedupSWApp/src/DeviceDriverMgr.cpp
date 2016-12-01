3bbed76aa73ea34122a54915a8a74faba7b980df
seikwon@kaist.ac.kr <seikwon@kaist.ac.kr>
2016-12-01 19:31:37 +0900

    device driver manager added

--- FPGA/DedupSWApp/src/DeviceDriverMgr.cpp
+++ FPGA/DedupSWApp/src/DeviceDriverMgr.cpp
@@ -1,4 +1,48 @@
 //
-// Created by seikwon on 16. 12. 1.
+// Created by chikdol on 16. 12. 1.
 //
 
+#include "DeviceDriverMgr.h"
+#include <cassert>
+
+DMADeviceDriverMgr::DMADeviceDriverMgr(int rxBufferSize, int txBufferSize){
+    m_txBufferSize = txBufferSize;
+    m_rxBufferSize = rxBufferSize;
+
+    m_axidmaDev = axidma_init();
+    assert(m_axidmaDev != NULL);
+
+    int txNumber, rxNumber;
+    int* txChannels;
+    int* rxChannels;
+
+    m_txBuffer = (char*) axidma_malloc(m_axidmaDev, txBufferSize);
+    txChannels = axidma_get_dma_tx(m_axidmaDev, &txNumber);
+    m_txChannel = txChannels[0];
+
+    m_rxBuffer = (char*) axidma_malloc(m_axidmaDev, rxBufferSize);
+    rxChannels = axidma_get_dma_rx(m_axidmaDev, &rxNumber);
+    m_rxChannel = rxChannels[0];
+}
+
+DMADeviceDriverMgr::~DMADeviceDriverMgr(){
+    axidma_free(m_axidmaDev, m_rxBuffer, m_rxBufferSize);
+    axidma_free(m_axidmaDev, m_txBuffer, m_txBufferSize);
+    axidma_destroy(m_axidmaDev);
+}
+
+char* DMADeviceDriverMgr::getTxBuffer(){
+    return m_txBuffer;
+}
+
+char* DMADeviceDriverMgr::getRxBuffer(){
+    return m_rxBuffer;
+}
+
+void DMADeviceDriverMgr::sendData(){
+    axidma_oneway_transfer(m_axidmaDev, AXIDMA_WRITE, m_txChannel, m_txBuffer, m_txBufferSize, true);
+}
+
+void DMADeviceDriverMgr::rcvData(){
+    axidma_oneway_transfer(m_axidmaDev, AXIDMA_READ, m_rxChannel, m_rxBuffer, m_rxBufferSize, true);
+}
--- FPGA/DedupSWApp/src/DeviceDriverMgr.h
+++ FPGA/DedupSWApp/src/DeviceDriverMgr.h
@@ -1,8 +1,42 @@
 //
-// Created by seikwon on 16. 12. 1.
+// Created by chikdol on 16. 12. 1.
 //
 
-#ifndef HEAD_DEVICEDRIVERMGR_H
-#define HEAD_DEVICEDRIVERMGR_H
+#ifndef HEAD_DMADEVICEDRIVERMGR_H
+#define HEAD_DMADEVICEDRIVERMGR_H
 
-#endif //HEAD_DEVICEDRIVERMGR_H
+#include <string>
+
+extern "C" {
+#include "libaxidma.h"
+}
+
+#define MODULE_NAME "/"
+#define TX_BUFFER_SIZE  8192
+#define RX_BUFFER_SIZE  56
+
+using namespace std;
+
+class DMADeviceDriverMgr{
+    public:
+        DMADeviceDriverMgr(int rxBufferSize = RX_BUFFER_SIZE, int txBufferSize = TX_BUFFER_SIZE);
+        ~DMADeviceDriverMgr();
+        char* getTxBuffer();
+        char* getRxBuffer();
+        void sendData();
+        void rcvData();
+
+    private:
+        axidma_dev_t m_axidmaDev;
+
+        int m_txBufferSize;
+        int m_rxBufferSize;
+
+        char* m_txBuffer;
+        char* m_rxBuffer;
+
+        int m_txChannel;
+        int m_rxChannel;
+};
+
+#endif //HEAD_DMADEVICEDRIVERMGR_H
